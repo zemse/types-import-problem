e8c0b2d35ba804edda0c08e357725da69dc0d9b1
zemse <sohamzemse143@gmail.com>
2020-09-12 02:01:01 +0530

    Run tsc main.ts

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var dependencies_1 = require("./dependencies");
+var hello = {
+    intensity: 123
+};
+console.log(hello.intensity, dependencies_1.constant);
